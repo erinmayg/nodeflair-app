594520117833888b3bc5eb5a50843775e0d36c2f
erinmayg <erinmaygunawan@gmail.com>
2021-02-16 05:24:24 +0800

    Edit search bar

--- src/components/JobSearch.js
+++ src/components/JobSearch.js
@@ -13,28 +13,12 @@ function JobSearch() {
       </div>
 
       <div className='job-search-bar-container'>
-        <div
-          role='combobox'
-          aria-haspopup='listbox'
-          aria-owns='react-autowhatever-1'
-          aria-expanded='false'
-          className='react-autosuggest__container'
-        >
-          <input
-            type='text'
-            autocomplete='off'
-            aria-autocomplete='list'
-            aria-controls='react-autowhatever-1'
-            className='react-autosuggest__input'
-            placeholder='Try: Frontend, QA Engineer, React.js'
-            value=''
-          ></input>
-          <div
-            id='react-autowhatever-1'
-            role='listbox'
-            className='react-autosuggest__suggestions-container'
-          ></div>
-        </div>
+        <input
+          type='text'
+          autocomplete='off'
+          placeholder='Try: Frontend, QA Engineer, React.js'
+          value=''
+        ></input>
         <button className='btn job-search-bar-button'>
           <FaSearch />
         </button>
